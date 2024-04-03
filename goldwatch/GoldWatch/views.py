876e8cde08608f50db51bb6c17549175e4555c85
aswin <aswinnathparat@gmail.com>
2024-04-03 10:11:04 +0530

    completed fuctionalities ,UI pending

--- goldwatch/GoldWatch/views.py
+++ goldwatch/GoldWatch/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from . models import GoldCard
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    records = GoldCard.objects.all()
+    return render(request,'index.html',{'records':records})
